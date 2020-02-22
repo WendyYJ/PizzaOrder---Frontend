b7fb28444f29090957ced7a9b06bdd3b9246fda0
zihan Mo <mozihan620@gmail.com>
2020-02-22 17:15:07 +1000

    about us add

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Information from './components/Infomation'
-
+import Middle from './components/Middle'
 const ShopCatalog = () => {
   
    return (
-
+      <div className="about-container">
   <Information/>
+  <Middle/>
+</div>
    );
 }
 
