2b9f165266c91de884a9b86d817c0c0b2229a35a
zihan Mo <mozihan620@gmail.com>
2020-02-24 15:55:33 +1000

    ingredient footer

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -3,7 +3,7 @@ import Header from '../PageLayout/Header/Header'
 import Information from './components/Infomation'
 import Middle from './components/Middle'
 import SecondaryHeader from '../PageLayout/Header/SecondaryHeader'
-
+import Ingredient from '../PageLayout/Footer/Ingredient'
 const AboutUs = () => {
   
    return (
@@ -12,6 +12,7 @@ const AboutUs = () => {
 <SecondaryHeader headername={"About Us"}/>
        <Information/>
        <Middle/>
+       <Ingredient/>
       </div>
    );
 }
---
+++ src/PageLayout/Footer/Ingredient.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import circle1 from '../../asset/img/circle1.png';
+import circle2 from '../../asset/img/circle2.png';
+import circle3 from '../../asset/img/circle3.png';
+import circle4 from '../../asset/img/circle4.png';
+import circle5 from '../../asset/img/circle5.png';
+import circle6 from '../../asset/img/circle6.png';
+import '../Style/Footer.scss'
+const Ingredient = () => {
+    return (
+        <div className="ingredientcontainer">
+            <div className="circlecontainer">
+<img src={circle1}></img>
+</div>
+<div className="circlecontainer">
+<img src={circle2}></img>
+</div>
+<div className="circlecontainer">
+<img src={circle3}></img>
+</div>
+<div className="circlecontainer">
+<img src={circle4}></img>
+</div>
+<div className="circlecontainer">
+<img src={circle5}></img>
+</div>
+
+<div className="circlecontainer">
+<img src={circle6}></img>
+</div>
+
+            </div>
+    );
+}
+
+
+export default Ingredient;
