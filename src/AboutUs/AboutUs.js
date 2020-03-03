3a3ff4b2a4a9ab6b99b628ae9c0734e8b63d1c6b
Linlin Deng <denglinlin315@gmail.com>
2020-03-03 23:20:03 +1000

    sidebar style changed

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import Header from '../PageLayout/Header/Header'
 import Information from './components/Infomation'
 import Middle from './components/Middle'
 import Question from './components/Question'
 import SecondaryHeader from '../PageLayout/Header/SecondaryHeader'
 import Ingredient from '../PageLayout/Footer/Ingredient'
+import Sidebar from '../PageLayout/Sidebar';
+import Toggle from '../PageLayout/Toggle';
 
-const AboutUs = () => {
+const AboutUs = ({children}) => {
+   const [sidebarOpen, setSidebarOpen] = useState(false)
+
+   const openHandler = () => {
+       if(!sidebarOpen){
+           setSidebarOpen(true);
+       }else{
+           setSidebarOpen(false);
+       }
+   }
+
+   const sidebarCloseHandler = () => {
+       setSidebarOpen(false);
+   }
+
+   let sidebar
+   if (sidebarOpen) {
+       sidebar = <Sidebar close={sidebarCloseHandler} sidebar={"sidebar"} />
+   }
+    
   
    return (
       <div className="about-container">
-       <Header/>
-       <SecondaryHeader headername={"About Us"}/>
-       <Information/>
-       <Middle/>
-       <Question/>
-       <Ingredient/>
+      <Fragment>
+         <Header/>
+         {sidebar}
+         <Toggle click={openHandler} />
+         <p>{children}</p>
+         <SecondaryHeader headername={"About Us"}/>
+         <Information/>
+         <Middle/>
+         <Question/>
+         <Ingredient/>
+       </Fragment>
       </div>
    );
 }
