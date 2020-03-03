4f2a9feeb2d95ccac72b3f78bdf108f9ca05e33d
Linlin Deng <denglinlin315@gmail.com>
2020-03-03 23:00:57 +1000

    sidebar added to other pages

--- src/PizzaMenu/SampleId.js
+++ src/PizzaMenu/SampleId.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import '.././PageLayout/Style/PageStyle.scss';
 import Header from '../PageLayout/Header/Header';
 import ThirdHeader from '../PageLayout/Header/ThirdHeader';
@@ -6,15 +6,41 @@ import HorizonBar from '../PageLayout/HorizonBar';
 import SampleMenu from './Components/SampleMenu';
 import ReviewForm from '../PageLayout/ReviewForm';
 import RelatedProduct from './Components/RelatedProduct';
+import Sidebar from '../PageLayout/Sidebar';
+import Toggle from '../PageLayout/Toggle';
 
-const MenuSamples = () => {
+const MenuSamples = ({children}) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+   const openHandler = () => {
+       if(!sidebarOpen){
+           setSidebarOpen(true);
+       }else{
+           setSidebarOpen(false);
+       }
+   }
+
+   const sidebarCloseHandler = () => {
+       setSidebarOpen(false);
+   }
+
+   let sidebar
+   if (sidebarOpen) {
+       sidebar = <Sidebar close={sidebarCloseHandler} sidebar={"sidebar"} />
+   }
+
   return (
     <div className ="MenuMain-container">
-      <Header />
-      <ThirdHeader headername={"Pizza Menu"} headername2={"Peri-Peri"} />
+      
+      <Fragment>
+        <Header />
+        {sidebar}
+        <Toggle click={openHandler}/>
+        <p>{children}</p>
+        <ThirdHeader headername={"Pizza Menu"} headername2={"Peri-Peri"} />
 
-      <HorizonBar className="horizonbar" />
-      <SampleMenu />
+        <HorizonBar className="horizonbar" />
+        <SampleMenu />
      
       <div className="horizonbar-Two">
         <p>DESCRIPTION</p>
@@ -24,6 +50,7 @@ const MenuSamples = () => {
       </div>
         <ReviewForm />
         <RelatedProduct />
+      </Fragment>
      </div>
   );
 }
--- src/ShoppingCart/ShoppingCart.js
+++ src/ShoppingCart/ShoppingCart.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import '.././PageLayout/Style/PageStyle.scss';
 import Header from '../PageLayout/Header/Header';
 import FourthHeader from '../PageLayout/Header/FourthHeader'
 import Cart from '../PageLayout/Cart';
+import Sidebar from '../PageLayout/Sidebar';
+import Toggle from '../PageLayout/Toggle';
 
 
-const ShoppingCart = () => {
+const ShoppingCart = ({children}) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+   const openHandler = () => {
+       if(!sidebarOpen){
+           setSidebarOpen(true);
+       }else{
+           setSidebarOpen(false);
+       }
+   }
+
+   const sidebarCloseHandler = () => {
+       setSidebarOpen(false);
+   }
+
+   let sidebar
+   if (sidebarOpen) {
+       sidebar = <Sidebar close={sidebarCloseHandler} sidebar={"sidebar"} />
+   }
+    
   return (
     <div className="shoppingCart-container">
+      <Fragment>
       <Header />
+      {sidebar}
+      <Toggle click={openHandler}/>
+      <p>{children}</p>
+
       <FourthHeader headername={"Shopping Cart"} style={{width: "100%"}}/>
-     <div className="shoppingCart1">
-     <div className="horizonbar-Three">
+      <div className="shoppingCart1">
+      <div className="horizonbar-Three">
         <p>PRODUCT</p>
         
         <span className="price"><p>PRICE</p></span>
@@ -21,6 +47,7 @@ const ShoppingCart = () => {
       </div>
         <Cart />
     </div>
+    </Fragment>
     </div>
 
   );
