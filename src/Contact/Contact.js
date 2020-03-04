bb9d9bdf4f7d77799fa0f77c70e31342abcd9044
Linlin Deng <denglinlin315@gmail.com>
2020-03-05 01:47:54 +1000

    solve conflict, google map added

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,21 +3,16 @@ import './Contact.scss';
 import '.././PageLayout/Style/PageStyle.scss';
 import Header from '../PageLayout/Header/Header';
 import SecondaryHeader from '../PageLayout/Header/SecondaryHeader';
-<<<<<<< HEAD
+
 import Sidebar from '../PageLayout/Sidebar';
 import Toggle from '../PageLayout/Toggle';
 import ContactMap from './ContactMap';
-=======
-//import Sidebar from '../PageLayout/Sidebar';
-//import Toggle from '../PageLayout/Toggle';
-// import ContactMap from './ContactMap';
->>>>>>> f1a846656dfcd5fd862a39d7ffb747bea7364d4f
+
 
 
 const Contact = ({children}) =>{
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
-<<<<<<< HEAD
     const openHandler = () => {
         if(!sidebarOpen){
             setSidebarOpen(true);
@@ -29,22 +24,12 @@ const Contact = ({children}) =>{
     const sidebarCloseHandler = () => {
         setSidebarOpen(false);
     }
-=======
-return (
-  <div className="contact-container">
-   
-        <Header />
-       
-    
-        <SecondaryHeader headername={"Contact Us"}/>
-        
-   
->>>>>>> f1a846656dfcd5fd862a39d7ffb747bea7364d4f
 
     let sidebar
     if (sidebarOpen) {
         sidebar = <Sidebar close={sidebarCloseHandler} sidebar={"sidebar"} />
     }
+
 
   return(
   <div className="contact-container">
@@ -59,6 +44,7 @@ return (
     </div>
   </Fragment>
   </div>
+
   );
 }
 
