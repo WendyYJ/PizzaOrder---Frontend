848a47ba3b6a942c87cd250ba302f9e1bd62f0b8
Andrea Li <45835344+z5231871@users.noreply.github.com>
2020-03-05 12:33:57 +1000

    adding carousel (worked)

--- src/Homepage/Carousel/Carousel.js
+++ src/Homepage/Carousel/Carousel.js
@@ -3,20 +3,22 @@ import React from "react";
 import Slider from "react-slick";
 import './Carousel.scss';
 import periperi from '../../asset/img/PeriPeri.png';
+import proni from '../../asset/img/proni.png';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 class Carousel extends React.Component{
   render() {
     var settings = {
-      dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplaySpeed:2000
     };
     return (
-      <Slider {...settings} className='homepage__menu__container'>
+      <Slider {...settings}  >
+        <div>
         <div className='homepage__menu'>
             <div className='homepage__menu-left'>
             <h2 className='homepage__menu-heading'>
@@ -37,11 +39,28 @@ class Carousel extends React.Component{
             <img src={periperi} alt='Peri Peri Pizza' />
             </div>
          </div>
-        <div className='homepage__menu '>
-          <img src='https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80' alt='showcase'/>
-        </div>
-        <div className='homepage__menu '>
-        <h3>3</h3>
+         </div>
+        <div>
+        <div className='homepage__menu'>
+            <div className='homepage__menu-left'>
+            <h2 className='homepage__menu-heading'>
+              Pepperoni
+            </h2>
+            
+            <p className='homepage__menu-price'>
+                $17-$23
+            </p>
+            <p className='homepage__menu-more'>
+                Marinated Chicken Breast Fillets, Shallots, Roasted Capsicum, Caramelised Onions & Bocconicini on a Tomato base, topped with Peri-Peri sauce.
+            </p>
+            <p className='homepage__menu-button'>
+                order now
+            </p>
+            </div>
+            <div className='homepage__menu-right'>
+            <img src={proni} alt='Pepperoni Pizza' />
+            </div>
+            </div>
         </div>
         <div className='homepage__menu'>
         <h3 >4</h3>
@@ -52,7 +71,9 @@ class Carousel extends React.Component{
         <div className='homepage__menu'>
           <h3>6</h3>
         </div>
+        
       </Slider>
+     
     );
   }  
 }
