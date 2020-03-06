f030488e68a56f13f5de3f4538a2f2aee0c10373
Andrea Li <45835344+z5231871@users.noreply.github.com>
2020-03-06 18:29:42 +1000

    finalise Carousel

--- src/Homepage/Carousel/Carousel.js
+++ src/Homepage/Carousel/Carousel.js
@@ -4,6 +4,9 @@ import Slider from "react-slick";
 import './Carousel.scss';
 import periperi from '../../asset/img/PeriPeri.png';
 import proni from '../../asset/img/proni.png';
+import Carni from '../../asset/img/olive.jpg';
+import mush from '../../asset/img/Shiitake.jpg';
+import vegan from '../../asset/img/valentinos.jpg';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
@@ -51,7 +54,7 @@ class Carousel extends React.Component{
                 $17-$23
             </p>
             <p className='homepage__menu-more'>
-                Marinated Chicken Breast Fillets, Shallots, Roasted Capsicum, Caramelised Onions & Bocconicini on a Tomato base, topped with Peri-Peri sauce.
+            Our singnature Pepperoni Pizza contains 834kJ a slice, which is pretty impressive given its comparably diminutive size. Please ordere this pizza with a standard 'classic crust'.
             </p>
             <p className='homepage__menu-button'>
                 order now
@@ -62,14 +65,69 @@ class Carousel extends React.Component{
             </div>
             </div>
         </div>
+        <div>
         <div className='homepage__menu'>
-        <h3 >4</h3>
+            <div className='homepage__menu-left'>
+            <h2 className='homepage__menu-heading'>
+              Shiitake Fusion
+            </h2>
+            
+            <p className='homepage__menu-price'>
+                $14-$22
+            </p>
+            <p className='homepage__menu-more'>
+            Order one of the standard 12-inch pizza range。 If you wolf down a whole one, you'll have chewed up 75% of your average daily intake in a single meal.             
+            </p>
+            <p className='homepage__menu-button'>
+                order now
+            </p>
+            </div>
+            <div className='homepage__menu-right'>
+            <img src={mush} alt='Shiitake Pizza' />
+            </div>
+            </div>
         </div>
+        <div>
         <div className='homepage__menu'>
-          <h3>5</h3>
+            <div className='homepage__menu-left'>
+            <h2 className='homepage__menu-heading'>
+              The Carnivore
+            </h2>
+            <p className='homepage__menu-price'>
+                $14-$22
+            </p>
+            <p className='homepage__menu-more'>
+            Some of you could easily wolf down a 12-inch pizza on your own. But with this meaty feast, you might want to reconsider. At 1120kJ a slice, it's not for the faint-hearted. 
+            </p>
+            <p className='homepage__menu-button'>
+                order now
+            </p>
+            </div>
+            <div className='homepage__menu-right'>
+            <img src={Carni} alt='Carnivore Pizza' />
+            </div>
+            </div>
         </div>
+        <div >
         <div className='homepage__menu'>
-          <h3>6</h3>
+            <div className='homepage__menu-left'>
+            <h2 className='homepage__menu-heading'>
+              Vegan Valentinos
+            </h2>
+            <p className='homepage__menu-price'>
+                $14-$22
+            </p>
+            <p className='homepage__menu-more'>
+            I’m not even that big of a cheese fan, but pizza just wasn’t pizza if there wasn’t a thick layer of mozzarella on top. Ya know? Thank goodness for me there is a 1-minute, homemade alternative to parmesan cheese. Vegan parmesan! Have you tried it?
+            </p>
+            <p className='homepage__menu-button'>
+                order now
+            </p>
+            </div>
+            <div className='homepage__menu-right'>
+            <img src={vegan} alt='the Vegan Pizza' />
+            </div>
+            </div>
         </div>
         
       </Slider>
