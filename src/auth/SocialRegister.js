70971c950dd3331da990b8672b6106e776771d05
Linlin Deng <denglinlin315@gmail.com>
2020-03-14 03:05:45 +1000

    modification

--- src/auth/SocialRegister.js
+++ src/auth/SocialRegister.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Style.scss";
 import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function SocialRegister() {
   return (
