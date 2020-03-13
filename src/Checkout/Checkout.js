28a62a41ada69c7db1fcbc0274c427b02d1a057f
Linlin Deng <denglinlin315@gmail.com>
2020-03-13 20:23:49 +1000

    checkout page added

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,5 +1,18 @@
 import React from "react";
+import './Checkout.scss';
+import LinkHeader from "../PageLayout/Header/LinkHeader";
+import PizzamenuSidebar from "../PageLayout/PizzamenuSidebar/PizzamenuSidebar";
+import ShoppingCartSidebar from "../PageLayout/ShoppingCartSidebar/ShoppingCartSidebar";
 
-const Checkout = () => <div />;
+
+const Checkout = () => (
+  <div className="checkout-container">
+    <PizzamenuSidebar />
+    <ShoppingCartSidebar />
+    <LinkHeader headername="Checkout" />
+
+  </div>
+);
+
 
 export default Checkout;
