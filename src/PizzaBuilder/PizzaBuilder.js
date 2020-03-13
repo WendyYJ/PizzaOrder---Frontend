2a609c1253b2ac29ed7449356b9dc7b411d11fc6
Andrea Li <45835344+z5231871@users.noreply.github.com>
2020-03-13 23:03:08 +1000

    init Expandables

--- src/PizzaBuilder/PizzaBuilder.js
+++ src/PizzaBuilder/PizzaBuilder.js
@@ -1,5 +1,16 @@
 import React from "react";
+import Accordion from "./Accordion/Accordion";
+import Footer from '../PageLayout/Footer/Footer';
 
-const PizzaBuilder = () => <div></div>;
+
+const PizzaBuilder = () => (
+    <div>
+        <Accordion/>
+        <Footer/>
+    </div>
+  
+  
+);
+
 
 export default PizzaBuilder;
