89ac626b4cfcb8946d2048e500523cc10e5223f2
Wendy Yu <y_jies@hotmail.com>
2020-03-17 13:28:36 +1000

    update

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import { createStore } from 'redux';
-||||||| 3e92a9f
-import { createStore } from "redux";
-=======
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../redux/reducers'
@@ -10,12 +5,5 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 
 
 export const store=createStore(reducers,composeWithDevTools(applyMiddleware(thunk)));
->>>>>>> 688506bfb4454bae7802f8091bca244dcd6b6ea7
 
-<<<<<<< HEAD
-import reducers from './reducers';
-||||||| 3e92a9f
-import reducers from "./reducers";
-=======
->>>>>>> 688506bfb4454bae7802f8091bca244dcd6b6ea7
 
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
-import { combineReducers } from 'redux';
-import questionReducer from './questionReducer';
-||||||| 3e92a9f
-import { combineReducers } from "redux";
-import questionReducer from "./questionReducer";
-=======
 import { combineReducers } from "redux";
 import questionReducer from "./questionReducer";
 import counterReducer from "./counterReducer";
 import pizzaReducer from "./pizzaReducer";
->>>>>>> 688506bfb4454bae7802f8091bca244dcd6b6ea7
 
 const reducers = combineReducers({
-<<<<<<< HEAD
-  question: questionReducer,
-||||||| 3e92a9f
-  question: questionReducer
-=======
   question: questionReducer,
   counter:counterReducer,
   pizza: pizzaReducer
->>>>>>> 688506bfb4454bae7802f8091bca244dcd6b6ea7
 });
 
 export default reducers;
