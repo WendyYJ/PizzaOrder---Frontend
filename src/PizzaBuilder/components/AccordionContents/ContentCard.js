6b5f98086b6571b77b664cc676769f3bde52f605
Andrea Li <45835344+z5231871@users.noreply.github.com>
2020-03-24 02:48:32 +1000

    adding selection effect

--- src/PizzaBuilder/components/AccordionContents/ContentCard.js
+++ src/PizzaBuilder/components/AccordionContents/ContentCard.js
@@ -1,31 +1,33 @@
 import React, { useState } from 'react';
 import './ContentCard.scss';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
 const ContentCard = props =>{
-    // const [descriptionIsShown, toggleisShown] = useState( true );
-    // const [isSelected, toggleisSelected] = useState( false );
-        
-    // toggleisShown(
-    //     descriptionIsShown === true ? false : true
-    // )
-        
-    // toggleisSelected(
-    //     isSelected === '' ? 'card-selected' : ''
-    // )
+   const [setSelected, setSelectedState] = useState('');
 
+    
+    function toggleSelection(){
+        setSelectedState(
+            setSelected === '' ? 'card-selected' : ''
+        )
+    }
+        
+
+    
 
     
         return(
-            <div className={`content`}
-            >
+            <div className={`content`} onClick={toggleSelection} >
                 <img className='content__img' src ={props.image} alt={props.name}/>
                 <h3 className='content__name'>{props.name}</h3>
-                <p className='content__description'>
-                    
-                        {props.description}
-                       
+                <p className='content__description'>  
+                   {
+                       setSelected === '' ?
+                            props.description
+                   : <div className={`${setSelected}`}> &#x2714; </div>
+                   }  
                 </p>
-                <p className='content__price'>$ {props.price}</p>
+                <p className='content__price'>${props.price}</p>
             </div>
         )
     
