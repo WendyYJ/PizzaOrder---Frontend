ae1aac2bf0654ac8e64bbb9c1dd021428b908433
Linlin <linlin@linlins-mbp.home>
2020-03-24 23:11:46 +1000

    modification

--- src/auth/SocialRegister.js
+++ src/auth/SocialRegister.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import './Style.scss';
 import { Button } from 'react-bootstrap';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-function SocialRegister() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+export default function SocialRegister() {
+  const classes = useStyles();
+  const preventDefault = event => event.preventDefault();
+
   return (
     <div className="socialRegister-container">
       <p className="socialRegister-title">Join Us With</p>
       <div className="socialRegister-link">
+      <Typography className={classes.root}>
         <Button className="social-link" id="facebook">
+          <Link href="https://www.facebook.com/" onClick={preventDefault} className="link">
           <span>FACEBOOK</span>
+          </Link>
+          
         </Button>
         <Button className="social-link" id="google">
-          <span>GOOGLE</span>
+          <Link href="https://www.google.com/" onClick={preventDefault} className="link">
+            <span>GOOGLE</span>
+          </Link>
         </Button>
         <Button className="social-link" id="paypal">
+         <Link href="https://www.paypal.com/au/home" onClick={preventDefault} className="link">
           <span>PAYPAL</span>
+        </Link>
         </Button>
+      
+      </Typography>
       </div>
     </div>
   );
 }
 
-export default SocialRegister;
+//export default SocialRegister;
